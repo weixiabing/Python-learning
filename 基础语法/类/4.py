663c2af78fbeac4f6d6c5711b0840e2c20959f79
“weixiabing” <weixiabing@hotmail.com>
2021-08-02 10:10:43 +0800

    1

--- 基础语法/类/4.py
+++ 基础语法/类/4.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Test:
     def __init__(self,age):#init 自动运行 
         self.age=age
@@ -6,7 +5,6 @@ class Test:
     score=100
 s1=Test(18)
 
-=======
 class Person:
     def __del__(self):
         print('销毁对象:{0}'.format(self))
@@ -14,4 +12,3 @@ p1=Person()
 p2=Person()
 del p2
 print(p2)
->>>>>>> e6bd8921c42a93e06abdc5401d36ec92bdeee0b6
