3717ace65277443d9e1ada1c0ae64cc4d8826a16
weixiabing <weixiabing@hotmail.com>
2021-08-06 21:21:04 +0800

    1

--- 数据结构与算法/1.py
+++ 数据结构与算法/1.py
@@ -4,6 +4,6 @@ for a in range(1001):
     for b in range(1001):
         for c in range(1001):
             if a+b+c==1000 and a**2+b**2==c**2:
-                print('a,b,c:',a,b,c)
+                print('a,b,c:',a,b,b)
 end_time=time.time()
 print('所用时间：',(end_time-start_time))
