77e9e70d8fe4b6f8ecc02faaf23f322836dbb235
“weixiabing” <weixiabing@hotmail.com>
2021-08-06 16:23:17 +0800

    1

--- 基础语法/5.py
+++ 基础语法/5.py
@@ -2,7 +2,7 @@
 print(list(range(3,-10,-1)))
 a=[x*2 for x in range(2) if x !=0]
 print(a)
-b=[[1,2,3],[2,3,4],[4,5,6]]
+b=[[1,2,2],[2,3,4],[4,5,6]]
 for m in range(3):
     for n in range(3):
         print(b[m][n],end='\t')
