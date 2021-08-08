3b5adbcb2e583b68b75266d1d0e492bac80dd548
“weixiabing” <weixiabing@hotmail.com>
2021-08-08 11:00:29 +0800

    1

--- main1.py
+++ main1.py
@@ -66,7 +66,7 @@ def main():
     # 替换 ---start--- 到 ---end--- 之间的内容
     # pytz.timezone('Asia/Shanghai')).strftime('%Y年%m月%d日%H时M分')
     fmt = '%Y-%m-%d %H:%M:%S %Z%z'
-    insert_info = "---开始---\n\n" + "更新时间:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')+"linux远程更新\n" + v2fy_info + "\n---结束---"
+    insert_info = "---开始---\n\n" + "更新时间:"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d %H:%M:%S')+"github action更新<br>\n" + v2fy_info + "\n---结束---"
     # 获取README.md内容
     with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
         readme_md_content = f.read()
