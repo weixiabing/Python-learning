eb4836b2d538e0e5a781bd173575b3c1298b30ac
Niko <weixiabing@hotmail.com>
2022-07-15 00:07:55 +0800

    1

--- vsc/3位数水仙花数.py
+++ vsc/3位数水仙花数.py
@@ -1,17 +1,17 @@
-a=1
-c=0
-for item in range(100,1000):
-    sum=0
-    c=item
+a = 1
+c = 0
+for item in range(100, 1000):
+    summ4 = 0
+    c = item
     for _ in range(3):
-        a=c%10
-        c//=10
-        #b=1 
-        sum+=a**3
+        a = c % 10
+        c //= 10
+        # b=1
+        summ4 += a ** 3
 
         '''for _ in range(3):
             b*=a
         sum+=b'''
-        
-    if item==sum:
-     print(item)
+
+    if item == summ4:
+        print(item)
