ebf23e5d0d6268bf509f8c0d240ff00ae317e9f2
Niko <weixiabing@hotmail.com>
2023-02-06 02:08:11 +0800

    Update function.py
    重构

--- snap/function.py
+++ snap/function.py
@@ -43,76 +43,50 @@ def load_pool_cards(data, cards_dict, pool):
     return acquired_cards, unacquired_cards
 
 
-def image_merge(acquired_cards, unacquired_cards, pool, output_file):  # 定义拼接图片函数
-    width = 310  # 设置图片宽度
-    height = 310  # 设置图片高度
+
+
+def image_merge(acquired_cards, unacquired_cards, pool, output_file):
+    width, height = 310, 310
     list = pool
-    acquired_cards_image_list = [Image.open('assets/acquired.webp')]
-    acquired_cards_image_list = acquired_cards_image_list+[Image.open(f'assets/{list}/{acquired_cards[i]}.webp')
-                                                           for i in range(len(acquired_cards))]  # 读取图片
-    unacquired_cards_image_list = [Image.open('assets/unacquired.webp')]
-    unacquired_cards_image_list = unacquired_cards_image_list+[Image.open(f'assets/{list}/{unacquired_cards[i]}.webp')
-                                                               for i in range(len(unacquired_cards))]  # 读取图片
-    acquired_cards_image_resized = [image.resize(
-        (width, height), Image.BILINEAR) for image in acquired_cards_image_list]
-    unacquired_cards_image_resized = [image.resize(
-        (width, height), Image.BILINEAR) for image in unacquired_cards_image_list]
-    # print(len(unacquired_cards_image_resized),len(acquired_cards_image_resized))  # 测试图片数量
-    blank_board = Image.new('RGBA', size=(
-        width*10, height*(((len(unacquired_cards_image_resized))//10+1)+((len(acquired_cards_image_resized))//10+1))))  # 新建空白画布
-    # print(blank_board.size[0], blank_board.size[1])  # 测试白板尺寸
-    boxs1 = []  # 用来存放黏贴图片位置（四至）
-    boxs2 = []  # 用来存放黏贴图片位置（四至）
-    # print(acquired_cards_image_resized)
-    for i in range(len(acquired_cards_image_resized)//10+1):
-        for j in range(10):
-            if i*10+j < len(acquired_cards_image_resized):
-                box = (j*width, i*height, (j+1)*width, (i+1)*height)
-                boxs1.append(box)
-    for index1, bo1 in enumerate(boxs1):  # 待黏贴的图片序号和位置
-        # 在空白板上对应位置黏贴对应图片
-        blank_board.paste(acquired_cards_image_resized[index1], bo1)
-    for i in range(len(unacquired_cards_image_resized)//10+1):
-        for j in range(10):
-            if i*10+j < len(unacquired_cards_image_resized):
-                box = (j*width, (i+len(acquired_cards_image_resized)//10+1)*height,
-                       (j+1)*width, ((i+len(acquired_cards_image_resized)//10+1)+1)*height)
-                boxs2.append(box)
-    for index2, bo2 in enumerate(boxs2):  # 待黏贴的图片序号和位置
-        # 在空白板上对应位置黏贴对应图片
-        blank_board.paste(unacquired_cards_image_resized[index2], bo2)
-    blank_board.save(output_file)  # 保存黏贴结果
+    
+    acquired_images = [Image.open('assets/acquired.webp')] + [Image.open(f'assets/{list}/{card}.webp') for card in acquired_cards]
+    unacquired_images = [Image.open('assets/unacquired.webp')] + [Image.open(f'assets/{list}/{card}.webp') for card in unacquired_cards]
+    
+    acquired_images = [img.resize((width, height), Image.BILINEAR) for img in acquired_images]
+    unacquired_images = [img.resize((width, height), Image.BILINEAR) for img in unacquired_images]
+    
+    rows = (len(acquired_images) + len(unacquired_images) - 1) // 10 + 1
+    board = Image.new('RGBA', (width * 10, height * rows))
+    
+    acquired_boxes = [(j * width, i * height, (j + 1) * width, (i + 1) * height) for i in range(len(acquired_images) // 10 + 1) for j in range(10) if i * 10 + j < len(acquired_images)]
+    unacquired_boxes = [(j * width, (i + len(acquired_images) // 10 + 1) * height, (j + 1) * width, ((i + len(acquired_images) // 10 + 1) + 1) * height) for i in range(len(unacquired_images) // 10 + 1) for j in range(10) if i * 10 + j < len(unacquired_images)]
+    
+    for index, img, box in zip(range(len(acquired_images)), acquired_images, acquired_boxes):
+        board.paste(img, box)
+    for index, img, box in zip(range(len(unacquired_images)), unacquired_images, unacquired_boxes):
+        board.paste(img, box)
+        
+    board.save(output_file)
+
 
 
 def save_json(pool, acquired_cards, unacquired_cards, output_file):
-    # 导入pool3_rank.json
-    if pool == 'pool-3':
-        with open('pool3_rank.json', 'r', encoding='utf-8') as f:
-            pool3_rank = f.read()
-            pool3_rank = json.loads(pool3_rank)
-            f.close()
-        keycards = {}
-        Combocards = {}
-        # keycards为unacquired_cards属于unacpool3_rank[keycards]的值
-        for key in pool3_rank['unacquired_card']['keycards']:
-            keycards = [
-                i for i in unacquired_cards if i in pool3_rank['unacquired_card']['keycards']]
-        # Combocards为unacquired_cards属于unacpool3_rank[Combocards]的值
-        for key in pool3_rank['unacquired_card']['Combocards']:
-            Combocards = [
-                i for i in unacquired_cards if i in pool3_rank['unacquired_card']['Combocards']]
-        # othercards为unacquired_cards不属于unacpool3_rank[keycards]和unacpool3_rank[Combocards]的值
-        othercards = [
-            i for i in unacquired_cards if i not in keycards and i not in Combocards]
-        # 导出json
+    if pool != 'pool-3':
         with open(output_file, 'w', encoding='utf-8') as f:
-            json.dump({'acquired_cards': acquired_cards, 'unacquired_cards': {'keycards': keycards,
-                    'Combocards': Combocards, 'othercards': othercards}}, f, ensure_ascii=False, indent=4)
-            f.close()
-    else:
-        with open(output_file, 'w', encoding='utf-8') as f:
-            json.dump({'acquired_cards': acquired_cards, 'unacquired_cards':unacquired_cards }, f, ensure_ascii=False, indent=4)
-            f.close()
+            json.dump({'acquired_cards': acquired_cards, 'unacquired_cards': unacquired_cards}, f, ensure_ascii=False, indent=4)
+        return
+
+    with open('pool3_rank.json', 'r', encoding='utf-8') as f:
+        pool3_rank = json.load(f)
+
+    keycards = [i for i in unacquired_cards if i in pool3_rank['unacquired_card']['keycards']]
+    combocards = [i for i in unacquired_cards if i in pool3_rank['unacquired_card']['Combocards']]
+    othercards = [i for i in unacquired_cards if i not in keycards and i not in combocards]
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump({'acquired_cards': acquired_cards, 'unacquired_cards': {'keycards': keycards,
+            'Combocards': combocards, 'othercards': othercards}}, f, ensure_ascii=False, indent=4)
+
 
 
 # 统计cards_dict中的值的数量
